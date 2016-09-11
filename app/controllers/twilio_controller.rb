30ebeac9dd6e63e9851d352956493f5309c4ec94
bacitracin <tracyctran@gmail.com>
2016-09-10 22:10:40 -0700

    send birthday text method is working

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,20 +6,20 @@ class TwilioController < ApplicationController
 
   def send_birthday_text
 
-  # credentials - hide later
-  account_sid = 'ACcd3fbbc32f31e6d2c425367a4db03155' 
-  auth_token = 'cff4a836142add66aaaa0b31db06a8b2' 
-  twilio_phone_number = "16264273517"
-   
-  # set up a client to talk to the Twilio REST API 
-  @client = Twilio::REST::Client.new account_sid, auth_token 
-   
-  @client.account.messages.create({
-    :from => twilio_phone_number, 
-    :to => '6262785522', 
-    :body => 'Happy birthday!', # add friend first name here 
-    :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg', # point to HBD card
-  })
+    # credentials - hide later
+    account_sid = 'ACcd3fbbc32f31e6d2c425367a4db03155' 
+    auth_token = 'cff4a836142add66aaaa0b31db06a8b2' 
+    twilio_phone_number = "16264273517"
+     
+    # set up a client to talk to the Twilio REST API 
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+     
+    @client.account.messages.create({
+      :from => twilio_phone_number, 
+      :to => '6262785522', # for testing, will be friend.phone_number
+      :body => 'Happy birthday!', # add friend.first_name
+      :media_url => 'https://raw.githubusercontent.com/sf-learners-hackathon-2016/hackathon-app/master/app/assets/images/birthdaycard.png', # point to HBD card
+    })
 
   end
 
