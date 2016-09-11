a1e81107c54d8dce1f9514b35342c20929807522
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-10 22:47:43 -0700

    updated User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name, :email, presence: true
-  validates :phone_number, presence: true 
+	has_secure_password
+	
+	has_many :friends
+
+  validates :first_name, 
+  				:last_name, 
+  				:email, 
+  				:phone_number,presence: true
+  				:password_digest, presence: true
+
+  
   validates :email, uniqueness: true
 end
