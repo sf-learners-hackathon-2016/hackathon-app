8b95fbd77cf3459393d6827534dd78e2b88182b7
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-11 00:05:44 -0700

    users controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
-  
-  def new
-    @user = User.new
+
+	before_action :set_user, only: [:edit, :update]
+
+  def edit 
+  	@users = User.all
   end
 
-  def create
+  def update
+    if @user.update(user_params)
+       flash[:notice] = "User profile successfully updated"
+       redirect_to @user
+     else
+       flash[:notice] = "Oops something went wrong. Please try again."
+       render :edit 
+     end
   end
+
+  def index
+  	@users = User.all
+  end
+
+  def show 
+    @user = User.find(params[:id])
+ end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation) 
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 
 end
+
--- db/migrate/0_create_users.rb
+++
@@ -1,6 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-    end
-  end
-end
