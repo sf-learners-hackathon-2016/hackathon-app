f601362c40adaa98672f44235aeb46e556588896
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-10 23:36:18 -0700

    added authy gem and devise for authentication for twilio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :authy_authenticatable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_secure_password
 	
 	has_many :friends
@@ -6,9 +10,8 @@ class User < ActiveRecord::Base
   validates :first_name, 
   				:last_name, 
   				:email, 
-  				:phone_number,presence: true
+  				:phone_number, 
   				:password_digest, presence: true
 
-  
-  validates :email, uniqueness: true
+  			
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911050250) do
+ActiveRecord::Schema.define(version: 20160911062357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,25 @@ ActiveRecord::Schema.define(version: 20160911050250) do
     t.string "last_name"
     t.string "email"
     t.string "phone_number"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",           default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "authy_id"
+    t.datetime "last_sign_in_with_authy"
+    t.boolean  "authy_enabled",           default: false
+    t.index ["authy_id"], name: "index_users_on_authy_id", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
